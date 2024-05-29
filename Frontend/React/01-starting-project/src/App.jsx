0b7cfe83bce7d304e81efef6633996372c3db033
Shahroz Khan <shahrozkha83@gmail.com>
2024-05-29 15:38:42 +0500

    24/05/2024

---
+++ Frontend/React/01-starting-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./component/header/header"
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/React/01-starting-project/src/component/header/header.jsx
@@ -0,0 +1,24 @@
+import "./header.css"
+
+function Header(){
+    return(
+      <>
+        <header>
+          <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+          <h1>React Essentials</h1>
+          <p>
+            Fundamental React concepts you will need for almost any app you are
+            going to build!
+          </p>
+        </header>
+      </>
+    )
+}
+  
+export default Header;
+
+
+<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sequi ex eaque quidem cum nemo 
+  ipsam sapiente ratione expedita explicabo at libero praesentium necessitatibus incidunt minima 
+  mollitia distinctio, quisquam, voluptas consequatur eius veniam dolorem totam, aspernatur sed? 
+  Ducimus modi repellendus incidunt, quod eligendi sapiente culpa a repellat nulla, excepturi fugit.</h1>
