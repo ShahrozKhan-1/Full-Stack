fc69ec080fa76c593b201d6d5e74c297f48921f0
Shahroz Khan <shahrozkha83@gmail.com>
2024-06-04 22:48:20 +0500

    4/6/2024

---
+++ Frontend/Project/coffo/src/components/navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./navbar.css";
+import logo from "./logo.png"
+
+function Navbar() {
+  return (
+    <div className='container'>
+        <img src={logo} />
+    </div>
+  )
+}
+
+export default Navbar
