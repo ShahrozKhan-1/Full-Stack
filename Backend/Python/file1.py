57461fc4f31bb65a4768dd5cfd58efdfc66a58b6
Shahroz Khan <shahrozkha83@gmail.com>
2024-07-16 14:04:05 +0500

    14/07/2024

---
+++ Backend/Python/file1.py
@@ -0,0 +1,31 @@
+# print("Hello, World!") #this is my first python lab 
+# count = 0
+# print(count)
+# ed = {'a':22, 'b':44}
+# print(ed['a'])
+# print(ed)
+
+# exset = {1, 2, 3, "hello"}
+# print(exset)
+
+# var = {1, 2, 3, "hello"}
+# print(type(var))
+
+# name = "john"
+# print(name[0]) 
+
+# print(len(name))
+
+# var1 = list(var)
+
+# print(type(var1))
+
+# num1 = int(input("Enter the first number: "))
+# num2 = float(input("Enter the second number: "))
+# add = num1+num2
+# print(type(add))
+
+a = True 
+b = True
+if a and b:
+    print("hello \n true")
---
+++ Backend/Python/file2.py
@@ -0,0 +1,49 @@
+# marks = int(input("Enter marks: "))
+
+# if(marks >= 85):
+#     print("A")
+    
+# elif(marks >= 80 and marks <= 84):
+#     print("A-")
+    
+# elif(marks >= 75 and marks <= 79):
+#     print("B+")
+    
+# elif(marks >= 70 and marks <= 74):
+#     print("B")
+    
+# elif(marks >= 65 and marks <= 69):
+#     print("C+")
+    
+# elif(marks >= 60 and marks < 64):
+#     print("C")
+    
+# elif(marks >= 55 and marks <= 59):
+#     print("D+")
+    
+# elif(marks >= 50 and marks <= 54):
+#     print("D")
+    
+# elif(marks < 50):
+#     print("F")
+    
+# else:
+#     print("Wrong input")  
+
+
+# http_status = int(input("Enter: "))
+
+# match (http_status):
+#     case 200:
+#         print("ok")
+#     case 404:
+#         print("page not found")
+#     case 300:
+#         print("redirection")
+#     case _:
+#         print("this is default")
+        
+desserts = ["pie", "cake", "custard"]
+    
+for dessert in desserts:
+    print("i love", dessert)
